57c725b066ebf4851b760ec6635f4f82d14cfa21
sobao92 <2mn69o0@gmail.com>
2022-02-26 16:37:03 +0900

    userモデルの記述ミスを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  validates :last_name, format{ with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
-  validates :first_name, format{ with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
-  validates :last_name_kana, format{ with: validates :last_name, format{ with: /\A[ァ-ヶー-]+\z/, message: 'は全角カナで入力してください' }
-  validates :first_name_kana, format{ with: validates :last_name, format{ with: /\A[ァ-ヶー-]+\z/, message: 'は全角カナで入力してください' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー-]+\z/, message: 'は全角カナで入力してください' }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー-]+\z/, message: 'は全角カナで入力してください' }
   
 end
