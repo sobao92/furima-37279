e5481ed46508585adb18110adf4d1c565fad678e
sobao92 <2mn69o0@gmail.com>
2022-03-05 15:06:17 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
   has_one_attached :image
 end
