d44872a1b233294eba46bc9ce0c724cbf3175cf7
sobao92 <2mn69o0@gmail.com>
2022-03-20 16:08:41 +0900

    ordersコントローラーを修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,16 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @item = Item.find(order_params[:item_id])
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(order_params[:item_id])
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    if @order_address.save
+    if @order_address.valid?
+      @order_address.save
       redirect_to root_path
     else
       render :index
