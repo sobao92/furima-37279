bb62140d031e8cf2cf9e4d6de37df44e885f3877
sobao92 <2mn69o0@gmail.com>
2022-03-20 16:13:29 +0900

    Formオブジェクトのモデル単体テストコードを記述

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    post_code { '123-4567' }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { Faker::Address.city }
+    block { Faker::Address.street_address }
+    building { '柳ビル103' }
+    phone_number { Faker::Number.decimal_part(digits: 10) }
+  end
+end
